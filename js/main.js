68e44e1810a6f7e96b26ffd0c1105903feabc983
“Alba” <albaginesgarcia25@gmail.com>
2023-09-29 11:00:47 +0200

    add paint and fetch functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 'use strict';
+////////// define variables
+
+const searchName = document.querySelector ('.js-series-input');
+const btnSeries = document.querySelector ('.js-btn-search');
+const seriesContainer = document.querySelector ('.js-series-list');
+const urlShow = '//api.tvmaze.com/search/shows?q=girls'; 
+
+
+/////////  data request
+
+fetch (urlShow)
+.then ((response) => response.json ())
+.then ((dataShow) => {
+    renderSeriesList (dataShow);
+});
+
+
+///////paint HTML structure 
+
+function renderSeries (series) {
+    let htmlStructure = "";
+
+    htmlStructure = `<li class="show">
+        <div>
+         <h2>${series.show.name}</h2>
+         <img title="" src="${series.show.image.medium}" alt="Es la imagen de la serie"/>
+        </div> 
+      </li>`;
+
+    return htmlStructure
+    
+}
+
+///////paint list 
+
+function renderSeriesList (showList) {
+    for (const series of showList) {
+        seriesContainer.innerHTML += renderSeries (series); 
+    }
+}
+
+
