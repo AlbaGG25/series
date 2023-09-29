be17339f71a350258b796565879007f581043d3f
“Alba” <albaginesgarcia25@gmail.com>
2023-09-29 12:19:21 +0200

    fixed images

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const btnSeries = document.querySelector ('.js-btn-search');
 const seriesContainer = document.querySelector ('.js-series-list');
 const urlShow = '//api.tvmaze.com/search/shows?q=girls'; 
 
+let series = [];
 
 /////////  data request
 
@@ -20,16 +21,21 @@ fetch (urlShow)
 
 function renderSeries (series) {
     let htmlStructure = "";
+   
+    htmlStructure += `<li class="show">
+    <div>
+    <h2>${series.show.name}</h2>`;
 
-    htmlStructure = `<li class="show">
-        <div>
-         <h2>${series.show.name}</h2>
-         <img title="" src="${series.show.image.medium}" alt="Es la imagen de la serie"/>
-        </div> 
-      </li>`;
+    if (series.show.image!==null){
+        htmlStructure +=`<img title="${series.show.name}" src="${series.show.image.medium}" alt="${series.show.name}"/>`
+      }else{
+        htmlStructure += `<img title="${series.show.name}" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="${series.show.name}"/>`;
+      }
+    htmlStructure += `</div>
+    </li>`;
 
     return htmlStructure
-    
+
 }
 
 ///////paint list 
@@ -40,4 +46,5 @@ function renderSeriesList (showList) {
     }
 }
 
+///// collect input value
 
